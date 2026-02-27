a304fbd4bca1906bf0933c4fdd39d3449772ce26
momenhei <moritz.menne@web.de>
2026-02-27 10:25:21 +0100

    added Virtual Horizon rounding

--- main.cpp
+++ main.cpp
@@ -13,21 +13,34 @@
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-std::array<SDL_Vertex,12> getGeometry(){
-    std::array<SDL_Vertex,12> vertices{};
+std::array<SDL_Vertex,30> getGeometry(){
+    std::array<SDL_Vertex,30> vertices{};
 
+    //calculating sclaing variables
     int width;
     int height;
     SDL_GetWindowSize(window, &width, &height);
     const float fWidth = static_cast<float>(width);
     const float fHeight = static_cast<float>(height);
-    const float spacer = fWidth*0.25;
+    const float hSpacer = fWidth*0.25;
+    const float vSpacer = fHeight*0.25;
     auto v = [&] (int i, float x, float y){vertices[i].position.x=x; vertices[i].position.y=y;vertices[i].color= {0.0f, 0.0f, 0.0f, 1.0f};};
 
-    v( 0,   0.0f,   0.0f    );   v( 1,  0.0f,           fHeight );  v(  2,  spacer,         fHeight);
-    v( 3,   spacer, 0.0f    );   v( 4,  0.0f,           0.0f    );  v(  5,  spacer,         fHeight);
-    v( 6,   fWidth, fHeight );   v( 7,  fWidth-spacer,  0.0f    );  v(  8,  fWidth-spacer,  fHeight);
-    v( 9,   fWidth, 0.0f    );   v( 10, fWidth,         fHeight );  v(  11, fWidth-spacer,  0.0f);
+    //left & right boundaries
+    v(  0,   0.0f,   0.0f    );   v( 1,  0.0f,           fHeight );  v(  2,  hSpacer,         fHeight);
+    v(  3,   hSpacer,0.0f    );   v( 4,  0.0f,           0.0f    );  v(  5,  hSpacer,         fHeight);
+    v(  6,   fWidth, fHeight );   v( 7,  fWidth-hSpacer, 0.0f    );  v(  8,  fWidth-hSpacer,  fHeight);
+    v(  9,   fWidth, 0.0f    );   v( 10, fWidth,         fHeight );  v(  11, fWidth-hSpacer,  0.0f   );
+
+    //upper rounding
+    v(  12,fWidth-hSpacer, 0.0f);   v( 13, fWidth/2, 0.0f );  v(14, fWidth-hSpacer,vSpacer);
+    v(  15,hSpacer, 0.0f);   v( 16, fWidth/2,0.0f );  v(17, hSpacer,vSpacer);
+    v(  18,fWidth, vSpacer/2);   v( 19, fWidth/2,0.0f );  v(20, 0.0f,vSpacer/2);
+
+    //lower rounding
+    v(  21,fWidth-hSpacer, fHeight);   v( 22, fWidth/2, fHeight);  v(23, fWidth-hSpacer,fHeight-vSpacer);
+    v(  24,hSpacer, fHeight);   v( 25, fWidth/2,fHeight );  v(26, hSpacer,fHeight-vSpacer);
+    v(  27,fWidth, fHeight-vSpacer/2);   v( 28, fWidth/2,fHeight );  v(29, 0.0f,fHeight-vSpacer/2);
 
     return vertices;
 }
